7f818584bd96595283e3e65bbc32b7718fa6c2da
John Mettraux <jmettraux@gmail.com>
2020-01-24 16:58:33 +0900

    Take frequency into account

--- lab/kanji_lists.rb
+++ lab/kanji_lists.rb
@@ -15,23 +15,23 @@ JSON.parse(File.read('var/kanji.json')).each do |k|
   lit = k['lit']
   grd = k['grd']
   jlpt = k['jlpt']
+  frq = k['frq']
 
   count += 1
-  known << lit if grd || jlpt
-  unknown << lit unless grd || jlpt
+  known << lit if grd || jlpt || frq
+  unknown << lit unless grd || jlpt || frq
 
   (index["g#{grd}"] ||= []) << lit if grd
   (index["j#{jlpt}"] ||= []) << lit if jlpt
 end
 
-index = index.inject({}) { |h, (k, v)| h[k] = v.join; h }
-
-pp index
-p count
-p known.size
-p unknown.size
-
-puts
-known.each { |lit| print lit }
-#unknown.each { |lit| print lit }
+#index = index.inject({}) { |h, (k, v)| h[k] = v.join; h }
+#
+#pp index
+puts "#{known.size}/#{count}"
+#p unknown.size
+#
+#puts
+#known.each { |lit| print lit }
+##unknown.each { |lit| print lit }
 
