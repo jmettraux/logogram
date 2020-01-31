9d96afafaf31bd6712cdd373668af99d4d6a56f3
John Mettraux <jmettraux@gmail.com>
2020-01-31 14:32:36 +0900

    Refine yomi kanji index

--- var/lib/kanji_index.rb
+++ var/lib/kanji_index.rb
@@ -14,10 +14,23 @@ by_onyomi = {}
 by_kunyomi = {}
 
 ks.each_with_index do |k, i|
+
   rds = k['rds'] || {}
-  (rds['ja_on'] || []).each { |r| (by_onyomi[r] ||= []) << i }
-  (rds['ja_kun'] || []).each { |r| (by_kunyomi[r] ||= []) << i }
+
+  (rds['ja_on'] || []).each { |r|
+    (by_onyomi[r] ||= []) << i }
+
+  (rds['ja_kun'] || []).each { |r|
+    (by_kunyomi[r] ||= []) << i
+    (by_kunyomi[r.split('.').first] ||= []) << i if r.index('.') }
 end
+
+by_onyomi = by_onyomi
+  .select { |k, _| k[0, 1] != '-' }
+  .sort_by { |k, _| k }
+by_kunyomi = by_kunyomi
+  .select { |k, _| k[0, 1] != '-' }
+  .sort_by { |k, _| k }
 
 h = {
   items: ks,
